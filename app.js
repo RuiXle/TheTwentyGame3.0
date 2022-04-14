d18d66dfe8832780b1985cb5fbb40767e4c68178
Xue <rxue@FVFZMY4QJ1WK.tld>
2022-04-14 08:42:09 -0500

    ButtonTranslate

---
+++ app.js
@@ -0,0 +1,5 @@
+function run() {
+    document.getElementById("b").style.left = "0px"
+    document.getElementById("b").style.top = "0px"
+    // document.getElementById("b").style.transform = "translateX(0px)"
+}
