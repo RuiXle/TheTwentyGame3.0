87be444ba587e6d96b2fc867ed7db0c888d7e75a
Betts <jbetts@FVFY376XJ1WK.tld>
2022-04-14 06:34:41 -0700

    Added Cards to js and game system. changed Id for card to cardUser and cardComp.

---
+++ app.js
@@ -0,0 +1,78 @@
+// After
+
+function getUserCard() {
+    var randUserNum = Math.floor(Math.random() * 10) + 1
+    var suitArray = ["C","D","H","S"]
+    var randSuit = Math.floor(Math.random() * 4)
+    document.getElementById("cardUser").innerHTML = "<img src=images/" + randNum + suitArray[randSuit] + ".jpg>" 
+    return randUserNum
+} 
+
+function userFun() {
+	var userTotal = 0;
+	var stand = "y";
+    do {
+		userTotal = userTotal + getUserCard();
+		stand = prompt(`You currently have ${userTotal}. Enter "y" to keep drawing cards and "n" to stand and stop drawing.`);
+		
+	} while ((stand == "y") && (stand != "n"));
+	return userTotal;
+}
+
+function getCompCard() {
+    var randCompNum = Math.floor(Math.random() * 10) + 1
+    var suitArray = ["C","D","H","S"]
+    var randSuit = Math.floor(Math.random() * 4)
+    document.getElementById("cardComp").innerHTML = "<img src=images/" + randNum + suitArray[randSuit] + ".jpg>" 
+    return randCompNum
+} 
+
+function comFun() {
+	var compTotal = 0;
+    do {
+		compTotal = compTotal + getCompCard();
+		alert(`The computer has ${compTotal}`)
+	} while ( compTotal <= 15);
+	return compTotal;
+}
+function compNum(com, user) {
+    if (user == com){
+		alert(`You has ${user} and the computer has ${com}. The round is tied No-one wins!`);
+		
+	}
+	if (user > 20){
+		alert(`You went over with ${user}`);
+		compwin = "y"
+		userwin = "n"
+	}
+	if (com > 20){
+		alert(`The computer went over with ${com}`);
+		userwin = "y"
+		compwin = "n"
+	}
+	if ((user > com) && (user <= 20)){
+		alert(`You win! You had ${user} and the computer had ${com}`)
+		userwin = "y"
+		compwin = "n"
+	}
+	if ((com > user) && (com <= 20)){
+		alert(`Sorry the computer won with ${com}, and You lost with ${user}`)
+		compwin = "y"
+		userwin = "n"
+	}
+	if (userwin == "y") {
+		wins = wins + 1
+	} 
+	if (compwin == "y"){
+		losses = losses + 1
+	}
+}
+
+function game() {
+	do{
+		retry = "n";
+		compNum( comFun(), userFun() );
+		alert(`You currently have won ${wins} matches and the computer has won a total of ${losses} times`)
+		retry = prompt(`Do you want to play again? Type "y" retry or "n" to stop`)
+	}while(retry == "y")
+}
