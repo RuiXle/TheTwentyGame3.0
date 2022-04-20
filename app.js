012c9f93619479bb27610da4c0a4a220cb15c57a
Xue <rxue@FVFZMY4QJ1WK.tld>
2022-04-20 08:46:04 -0500

    checkpoint4

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ function run() {
     document.getElementById("b").style.top = "0px";
     document.getElementById("b").style.width = "100%";
     document.getElementById("b").style.transform = "translateX(0px)";
+
+	//botton C
+	document.getElementById("c").style.left = "0px"
+	document.getElementById("c").style.top = "0px"
+	document.getElementById("c").style.display = "block";
 }
 // After
 
@@ -17,6 +22,15 @@ function getUserCard() {
 function game(){
 	getUserCard();
 }
+
+
+
+
+
+
+
+
+
 /*
 function userFun() {
 	var userTotal = 0;
